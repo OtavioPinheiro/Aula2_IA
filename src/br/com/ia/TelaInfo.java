5ddb24a3abc84e10a2bcf2ed968a2c33ac189e21
Otávio Augusto Pinheiro <otavioaecpinheiro@gmail.com>
2022-03-01 19:10:32 -0300

    Initial commit

---
+++ src/br/com/ia/TelaInfo.java
@@ -0,0 +1,46 @@
+package br.com.ia;
+
+import javax.swing.*;
+
+public class TelaInfo {
+    private JLabel Titulo;
+    private JComboBox comboVoador;
+    private JLabel labelPelo;
+    private JLabel labelPenas;
+    private JLabel labelOvos;
+    private JLabel labelLeite;
+    private JLabel labelVoador;
+    private JLabel labelAquatico;
+    private JLabel labelPredador;
+    private JLabel labelDentes;
+    private JLabel labelVertebra;
+    private JLabel labelAr;
+    private JLabel labelVeneno;
+    private JLabel labelPernas;
+    private JLabel labelRabo;
+    private JLabel labelDomestico;
+    private JLabel labelAgil;
+    private JLabel labelBarbatanas;
+    private JComboBox comboPelo;
+    private JComboBox comboPenas;
+    private JComboBox comboOvos;
+    private JComboBox comboLeite;
+    private JComboBox comboAquatico;
+    private JComboBox comboPredador;
+    private JComboBox comboDentes;
+    private JComboBox comboVertebra;
+    private JComboBox comboAr;
+    private JComboBox comboVenenoso;
+    private JComboBox comboBarbatanas;
+    private JComboBox comboPernas;
+    private JComboBox comboRabo;
+    private JComboBox comboDomestico;
+    private JComboBox comboAgil;
+    private JButton confirmarButton;
+
+    public TelaInfo() {
+        //Instanciar a classe de informações
+
+
+    }
+}
